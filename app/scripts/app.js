55bbc7c4307a5eabdcddcfcb38ceded26c6c7918
Paul Nelligan <nellboy@gmail.com>
2016-01-05 15:37:37 +0100

    install angular scaffold using Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name messageBoardApp
+ * @description
+ * # messageBoardApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('messageBoardApp', [
+    'ngResource'
+  ]);
