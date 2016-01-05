da2822bac88782a5f0d9aa490ce6aa9812ff2f22
Paul Nelligan <nellboy@gmail.com>
2016-01-05 21:22:19 +0100

    add Message form and functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,7 @@
  */
 angular.module('messageBoardApp')
   .controller('MainCtrl', function ($scope) {
+    this.newMessage = {'poster': '', 'message': '', 'errors': []};
     this.messages = [];
 
     this.collectMessages = function(){
@@ -16,6 +17,26 @@ angular.module('messageBoardApp')
         this.messages = json.messages;
         $scope.$apply();
       }.bind(this));
+    };
+
+    this.addMessage = function(){
+      this.newMessage.errors = [];
+      if(!this.newMessage.poster.length){
+        this.newMessage.errors.push('Your name is required');
+      }
+      if(this.newMessage.poster.length > 50){
+        this.newMessage.errors.push('Your name is too long, 50 characters maximum is allowed.');
+      }
+      if(!this.newMessage.message.length){
+        this.newMessage.errors.push('Your message is required');
+      }
+      if(this.newMessage.message.length > 200){
+        this.newMessage.errors.push('Your message is too long, 200 characters maximum is allowed.');
+      }
+      if(!this.newMessage.errors.length){
+        this.messages.push(this.newMessage);
+        this.newMessage = {'poster': '', 'message': '', 'errors': []};
+      }
     };
   })
 
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -13,13 +13,11 @@ describe('Controller: MainCtrl', function () {
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
-
     spyOn(MainCtrl, 'collectMessages').and.callFake(function () {
       this.messages = [
         {
@@ -33,11 +31,46 @@ describe('Controller: MainCtrl', function () {
       ];
     });
 
+    MainCtrl.collectMessages();
   }));
 
   it('should collect messages', function () {
-    MainCtrl.collectMessages();
     expect(MainCtrl.messages.length).toBe(2);
+  });
+
+  it('should allow visitors to add valid messages', function () {
+    MainCtrl.newMessage = {"poster": "Paul Nelligan", "message": "Hello World, again!"};
+    MainCtrl.addMessage();
+    expect(MainCtrl.messages.length).toBe(3);
+    expect(MainCtrl.newMessage.errors.length).toBe(0);
+  });
+
+  it('should prevent visitors from adding messages with no poster', function () {
+    MainCtrl.newMessage = {"poster": "", "message": "Hello World, again!"};
+    MainCtrl.addMessage();
+    expect(MainCtrl.messages.length).toBe(2);
+    expect(MainCtrl.newMessage.errors.length).toBe(1);
+  });
+
+  it('should prevent visitors from adding messages with no messages', function () {
+    MainCtrl.newMessage = {"poster": "Paul Nelligan", "message": ""};
+    MainCtrl.addMessage();
+    expect(MainCtrl.messages.length).toBe(2);
+    expect(MainCtrl.newMessage.errors.length).toBe(1);
+  });
+
+  it('should prevent visitors from adding messages with poster longer than 50 characters', function () {
+    MainCtrl.newMessage = {"poster": "Paul Nelligan Paul Nelligan Paul Nelligan Paul Nelligan", "message": "Hello World, again!"};
+    MainCtrl.addMessage();
+    expect(MainCtrl.messages.length).toBe(2);
+    expect(MainCtrl.newMessage.errors.length).toBe(1);
+  });
+
+  it('should prevent visitors from adding messages with message longer than 200 characters', function () {
+    MainCtrl.newMessage = {"poster": "Paul Nelligan", "message": "Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again! Hello World, again!"};
+    MainCtrl.addMessage();
+    expect(MainCtrl.messages.length).toBe(2);
+    expect(MainCtrl.newMessage.errors.length).toBe(1);
   });
 
 });
