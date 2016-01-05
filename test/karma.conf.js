c9d881fc8bbf08cfe74f179a349327da8842336a
Paul Nelligan <nellboy@gmail.com>
2016-01-05 15:50:28 +0100

    installs angular bootstrap with bower

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
